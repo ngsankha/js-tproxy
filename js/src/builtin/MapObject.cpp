4148c7df188b6feff76ffab1f6dfa296f1d4a706
Sankha Narayan Guria <sankha93@gmail.com>
2014-06-30 10:33:22 +0200

    Hash TransparentProxy objects according to their target object

--- js/src/builtin/MapObject.cpp
+++ js/src/builtin/MapObject.cpp
@@ -808,6 +808,8 @@ HashableValue::hash() const
     // HashableValue::setValue normalizes values so that the SameValue relation
     // on HashableValues is the same as the == relationship on
     // value.data.asBits.
+    if (value.isObject())
+        return DefaultHasher<JSObject *>::hash(GetIdentityObject(NULL, &value.toObject()));
     return value.asRawBits();
 }
 
@@ -815,7 +817,19 @@ bool
 HashableValue::operator==(const HashableValue &other) const
 {
     // Two HashableValues are equal if they have equal bits.
-    bool b = (value.asRawBits() == other.value.asRawBits());
+    JS::Value val;
+    JS::Value otherVal;
+    if (value.isObject())
+        val = OBJECT_TO_JSVAL(GetIdentityObject(NULL, &value.toObject()));
+    else
+        val = value;
+
+    if (other.value.isObject())
+        otherVal = OBJECT_TO_JSVAL(GetIdentityObject(NULL, &other.value.toObject()));
+    else
+        otherVal = other.value;
+
+    bool b = (val == otherVal);
 
 #ifdef DEBUG
     bool same;
